0edb9610ba3d8f4c1b0f3ea5ab1b4bcee95e8260
Dan Ranges <danranges@gmail.com>
2020-12-01 20:48:51 +0100

    Add player choice error handling, playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,7 @@
+const choices = ['rock', 'paper', 'scissors'];
+
 function computerPlay() {
-    let computerMove = ['rock', 'paper', 'scissors'];
-    return computerMove[randomMove(0, 2)];
+    return choices[randomMove(0, 2)];
 }
 
 function randomMove(min, max) {
@@ -8,9 +9,39 @@ function randomMove(min, max) {
 }
 
 function playerPlay() {
-    let playerMove = window.prompt("Rock, paper, or scissors?");
-    return playerMove.toLowerCase();
+    let playerMove = window.prompt('Rock, paper, or scissors?');
+
+    if (choices.includes(playerMove.toLowerCase())) {
+        return playerMove.toLowerCase();
+    } else {
+        alert ('Please enter a valid selection.')
+        playerPlay()
+    }
 }
 
+function playRound(computerSelection, playerSelection) {
+    if (computerSelection == playerSelection) {
+        console.log(`It's a tie! You both picked ${computerSelection}! Please pick again.`)
+        playRound()
+    } else if (computerSelection == 'rock' && playerSelection == 'paper') {
+        window.prompt('Paper covers rock! You win!')
+        return 'win';
+    } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
+        
+        return 'lose';
+    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
 
-//console.log(playerPlay());
+        return 'lose';
+    } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
+
+        return 'win';
+    } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
+
+        return 'win';
+    } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
+
+        return 'lose';
+    }
+
+}
+
